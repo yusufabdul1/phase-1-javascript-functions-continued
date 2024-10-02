4645d8fc5fcd974faf64302db963e3f73d99eda7
Yusuf Abdul <yusufabdul212112@gmail.com>
2024-10-02 15:28:28 +0300

    adds new file

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+const mondayWork = function(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(flair = '*'){
+    return function(adjective = 'special'){
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
 // code your solution here
